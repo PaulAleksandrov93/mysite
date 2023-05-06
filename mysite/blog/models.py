4862c7f94277bbb20d194fb4b07834dca5ef47e5
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-05-06 20:01:36 +0300

    add forms and forms handler

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,34 +5,42 @@ from django.urls import reverse
 
 # Create your models here.
 
-class PublishedManager(models.Manager):
+class PublishedManager(models.Manager): 
     def get_queryset(self): 
-        return super().get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published')
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-    objects = models.Manager()
-    published = PublishedManager()
 
-    class Meta:
-        ordering = ('-publish',)
+class Post(models.Model): 
+    STATUS_CHOICES = ( 
+        ('draft', 'Draft'), 
+        ('published', 'Published'), 
+    ) 
+    title = models.CharField(max_length=250) 
+    slug = models.SlugField(max_length=250,  
+                            unique_for_date='publish') 
+    author = models.ForeignKey(User, 
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts') 
+    body = models.TextField() 
+    publish = models.DateTimeField(default=timezone.now) 
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    status = models.CharField(max_length=10,  
+                              choices=STATUS_CHOICES, 
+                              default='draft') 
     
-    def __str__(self):
+    objects = models.Manager() # The default manager. 
+    published = PublishedManager() # Our custom manager.
+
+    class Meta: 
+        ordering = ('-publish',) 
+
+    def __str__(self): 
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args = [self.publish.year,
-                        self.publish.month, self.publish.day, self.slug])
-    
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
